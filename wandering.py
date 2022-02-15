3e4ba9bcfa62d9e5063b5829420c0a6644a36637
Sky <hhernandez144@misena.edu.co>
2022-02-15 16:00:35 -0500

    Uno de tantos intentos

--- wandering.py
+++ wandering.py
@@ -10,5 +10,5 @@ class ComunWandering(Wandering):
     def __init__(self, name):
         super().__init__(name)
         def walk(self):
-            return random.choice([(0, 1), (0, -1), (1, 0), (-1, 0)])
+            return random.choice([(0, 3), (0, -3), (3, 0), (-3, 0)])
     
