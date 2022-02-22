07ea3b9b7f5d3ab59f6d5faadfd006a97a3eb37d
Sky <hhernandez144@misena.edu.co>
2022-02-22 15:36:14 -0500

    Creamos la funcion main y el enter point

--- randomTrack.py
+++ randomTrack.py
@@ -1,3 +1,4 @@
+from turtle import distance
 from wandering import ComunWandering, Wandering
 from track import Track 
 from location import Location
@@ -28,3 +29,23 @@ def graph(x, y):
     graphics = figure(title='Camino del Errante', x_axis_label='Pasos', y_axis_label='Distancia')
     graphics.line(x. y, legend='Distancia')
     show(graphics)
+    
+def main(distances_walk, number_attemps, type_wandering):
+    average_walking_distance = []
+    
+    for steps in distances_walk:
+        distances = simulate_walk(steps, number_attemps, type_wandering)
+        middle_distance = roun(sum(distances) / len(distances), 4)
+        max_distances= max(distances)
+        min_distances = min(distances)
+        average_walking_distance.append(middle_distance)
+        print(f'{type_wandering.__name__} Caminata aleatoria de {steps} pasos')
+        print(f'Media = {middle_distance}')
+        print(f'Max = {max_distances}')
+        print(f'Min = {min_distances}')
+    graph(distances_walk, average_walking_distance)
+    
+if __name__ == '__main__':
+    distance_walk = [10, 100, 1000, 10000]
+    number_attemps = 100
+    main(distance_walk, number_attemps, ComunWandering)
