f0f8654dc6385a86983028edb058fec9ed2c4072
Sky <hhernandez144@misena.edu.co>
2022-03-08 15:42:48 -0500

    Idnetamos la funcion walk de wandering

--- wandering.py
+++ wandering.py
@@ -9,6 +9,7 @@ class ComunWandering(Wandering):
 
     def __init__(self, name):
         super().__init__(name)
-        def walk(self):
-            return random.choice([(0, 3), (0, -3), (3, 0), (-3, 0)])
+        
+    def walk(self):
+        return random.choice([(0, 3), (0, -3), (3, 0), (-3, 0)])
     
